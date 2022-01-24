8acb99443126eefd46855e6f21adeee452561714
Chi-kwan Chan <chanc@arizona.edu>
2022-01-24 16:56:35 -0700

    Update lecture 2 and hands-on

---
+++ 02/hello.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+#
+# ASSIGNMENT: print "hello world" from this script
+#
+# Hences on how to print strings from python can be found in the
+# hands-on note:
+#
+#     https://github.com/uarizona-2022spring-phys105a/phys105a/blob/main/01/Handson.ipynb
+#
+print("hello world")
