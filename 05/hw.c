8d167aa0e3eb343cbe870cf35b1b3dd76a6b01c2
Chi-kwan Chan <chanc@arizona.edu>
2022-02-13 10:57:35 -0700

    Update the lecture

---
+++ 05/hw.c
@@ -0,0 +1,6 @@
+#include <stdio.h> /* I/O is not built-in to the language; include standard library */
+int main()         /* a program always uses `main` as an entry point */
+{
+	printf("Hello World!\n"); /* use "format-print" for output */
+	return 0;                 /* the standard is to return 0 from main if no error */
+}
